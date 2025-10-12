aefb15f1fec3788783df9e9c1c6165bf634e4b17
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-12 12:06:38 +0100

    Update solution - CodeHub

--- 8-kyu/never-visit-a/never-visit-a.js
+++ 8-kyu/never-visit-a/never-visit-a.js
@@ -1,19 +1,5 @@
-const fruits = [
-  "kiwi", "pear", "kiwi", "banana", "melon", "banana", "melon", "pineapple", "apple", "pineapple",
-  "cucumber", "pineapple", "cucumber", "orange", "grape", "orange", "grape", "apple", "grape", "cherry",
-  "pear", "cherry", "pear", "kiwi", "banana", "kiwi", "apple", "melon", "banana", "melon",
-  "pineapple", "melon", "pineapple", "cucumber", "orange", "apple", "orange", "grape", "orange", "grape",
-  "cherry", "pear", "cherry", "pear", "apple", "pear", "kiwi", "banana", "kiwi", "banana",
-  "melon", "pineapple", "melon", "apple", "cucumber", "pineapple", "cucumber", "orange", "cucumber", "orange",
-  "grape", "cherry", "apple", "cherry", "pear", "cherry", "pear", "kiwi", "pear", "kiwi",
-  "banana", "apple", "banana", "melon", "pineapple", "melon", "pineapple", "cucumber", "pineapple", "cucumber",
-  "apple", "grape", "orange", "grape", "cherry", "grape", "cherry", "pear", "cherry", "apple",
-  "kiwi", "banana", "kiwi", "banana", "melon", "banana", "melon", "pineapple", "apple", "pineapple"
-];
-​
 function subtractSum(n){
-  let sum = String(n).split('').map(Number).reduce((acc, val) => acc + val);
-  n -= sum;
-  if (n > 100)    return subtractSum(n);
-  else            return fruits[n-1]; 
+  // when summing all the digits the subtracting it from n
+  // it will always be equal to a 9 MULtiple which is always "apple"
+  return "apple";
 }
