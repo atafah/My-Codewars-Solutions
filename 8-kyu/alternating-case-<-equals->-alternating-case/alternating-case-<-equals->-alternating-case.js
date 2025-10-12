61c44942eb03330ca9b37535582038b5422594ea
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-12 15:26:30 +0100

    Update solution - CodeHub

--- 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.js
+++ 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.js
@@ -1,14 +1,3 @@
 String.prototype.toAlternatingCase = function () {
-  let str = this.toString();
-  let alter = "";
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === str[i].toUpperCase() && str[i] !== str[i].toLowerCase())
-      alter += str[i].toLowerCase();
-    else if (str[i] === str[i].toLowerCase() && str[i] !== str[i].toUpperCase())
-      alter += str[i].toUpperCase();
-    else
-      alter += str[i];
-  }
-  
-  return alter;
+  return this.split('').map(c => c === c.toUpperCase() ? c.toLowerCase(): c.toUpperCase()).join('');
 }
