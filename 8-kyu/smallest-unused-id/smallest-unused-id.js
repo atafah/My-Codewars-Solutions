21c7e44ee92f82d6b296fa3389c62368f5f498c6
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-24 11:50:46 +0100

    Update solution - CodeHub

--- 8-kyu/smallest-unused-id/smallest-unused-id.js
+++ 8-kyu/smallest-unused-id/smallest-unused-id.js
@@ -1,12 +1,7 @@
  
 function nextId(ids){
-  let arr = [...new Set(ids)].sort((a, b) => a - b);
-  if (arr[0] !== 0)
-    return 0;
-  
-  for (let i = 0; i < arr.length; i++)
-  {
-    if (arr[i]+1 !== arr[i+1] || i === arr.length - 1)
-      return arr[i]+1;
-  }
+  let number = 0;
+  while (number.includes(ids))
+    number++;
+  return number;
 }
