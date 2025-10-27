760a3c9cb8c24013575699b55a3afc6c0596c112
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-27 18:16:49 +0100

    Add solution - CodeHub

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,6 @@
+ 
+function quote(fighter) {
+  if (fighter[0].toLowerCase() === 'g')
+    return "I am not impressed by your performance."
+  return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
