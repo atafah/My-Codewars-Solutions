d68d95b64efd7f6f38ae22e2b1927424b1278336
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-28 21:02:39 +0100

    Add solution - CodeHub

---
+++ 8-kyu/cat-years-dog-years/cat-years-dog-years.js
@@ -0,0 +1,10 @@
+ 
+var humanYearsCatYearsDogYears = function(humanYears) {
+  if (humanYears === 1) {
+    return [humanYears, 15, 15]
+  } else if (humanYears === 2) {
+    return [humanYears, 24, 24];
+  }
+  return [humanYears, 24+(humanYears-2)*4, 24+(humanYears-2)*5];
+}
+​
