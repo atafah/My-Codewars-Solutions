b7b167b04624a0c01702fdb4830144e867425b31
anas tafah <154932472+atafah@users.noreply.github.com>
2025-10-29 10:55:00 +0100

    Update solution - CodeHub

--- 8-kyu/drink-about/drink-about.js
+++ 8-kyu/drink-about/drink-about.js
@@ -1,5 +1,8 @@
- 
 function peopleWithAgeDrink(age) {
+//Surah al-Anbiya
+//30. Do the unbelievers not see that the heavens and the earth were joined together, 
+//and We split them apart? And We made from water every living thing. 
+//Will they not believe?
+  
   return age >= 21 ? "drink whisky" : age >= 18 ? "drink beer" : age >= 14 ? "drink coke" : "drink toddy";
-​
 };
