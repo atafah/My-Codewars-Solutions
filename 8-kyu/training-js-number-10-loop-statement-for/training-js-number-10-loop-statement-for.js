340e6f7beaa3bf594650465c331c87f22c45df92
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-07 21:02:51 +0100

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-10-loop-statement-for/training-js-number-10-loop-statement-for.js
@@ -0,0 +1,10 @@
+function pickIt(arr){
+  let odd = [], even = [];
+  
+  for (let i = 0; i < arr.length; i++){
+    arr[i] % 2 ? odd.push(arr[i]) : even.push(arr[i]);
+  }
+  
+  
+  return [odd,even];
+}
