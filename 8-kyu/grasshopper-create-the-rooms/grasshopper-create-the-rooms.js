3fc4f7a77c4f65a9e9719da5be0f63910b126f16
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-14 17:48:39 +0100

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-create-the-rooms/grasshopper-create-the-rooms.js
@@ -0,0 +1,21 @@
+ 
+var rooms = {
+  room1: {
+    name: "pray room",
+    description: "room to pray",
+    completed: "yes"
+  },
+​
+  room2: {
+    name: "office room",
+    description: "room to work",
+    completed: "yes"
+  },
+​
+  room3: {
+    name: "bedroom",
+    description: "room to sleep",
+    completed: "yes"
+  }
+};
+​
