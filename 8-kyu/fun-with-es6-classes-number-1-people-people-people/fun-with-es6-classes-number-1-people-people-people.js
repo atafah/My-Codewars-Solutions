6fbe485c8ea33d82f3f3e3b2a8df650632ff321d
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-14 17:58:18 +0100

    Add solution - CodeHub

---
+++ 8-kyu/fun-with-es6-classes-number-1-people-people-people/fun-with-es6-classes-number-1-people-people-people.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(firstName="John", lastName="Doe", age=0, gender="Male"){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+  
+  static greetExtraTerrestrials(raceName){
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+  
+  sayFullName(){
+    return this.firstName + " " + this.lastName;
+  }
+  
+}
