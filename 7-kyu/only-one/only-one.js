084d136a6daf135a0447b6a7d91680cfbaffa389
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-15 19:22:24 +0100

    Update solution - CodeHub

--- 7-kyu/only-one/only-one.js
+++ 7-kyu/only-one/only-one.js
@@ -2,5 +2,5 @@
 function onlyOne(...args) {
   const trueCount = args.filter(b => b).length;
   const falseCount = args.filter(b => !b).length;
-  return Math.min(trueCount, falseCount) === 1;
+  return trueCount === 1;
 }
