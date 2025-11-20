e18de620541fbb291078d8c4e5a144b465762a08
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-20 23:32:34 +0100

    Add solution - CodeHub

---
+++ 8-kyu/multiple-of-index/multiple-of-index.js
@@ -0,0 +1,8 @@
+function multipleOfIndex(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i] % (i) === 0) || (i === 0 && array[i] === 0))
+      newArr.push(array[i]);
+  }
+  return newArr;
+}
