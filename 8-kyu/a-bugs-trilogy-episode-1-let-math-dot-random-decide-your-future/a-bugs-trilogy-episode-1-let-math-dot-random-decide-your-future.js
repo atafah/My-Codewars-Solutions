c92c7f531d5720160f1adf2a8198cf80632f3e04
anas tafah <154932472+atafah@users.noreply.github.com>
2025-11-21 20:48:38 +0100

    Add solution - CodeHub

---
+++ 8-kyu/a-bugs-trilogy-episode-1-let-math-dot-random-decide-your-future/a-bugs-trilogy-episode-1-let-math-dot-random-decide-your-future.js
@@ -0,0 +1,8 @@
+function yourFutureCareer() {
+  var career = Math.random();
+  if (career <= 0.32) 
+    return "FrontEnd Developer";
+  if (career <= 0.65) 
+    return "BackEnd Developer";
+  return 'Full-Stack Developer';
+}
